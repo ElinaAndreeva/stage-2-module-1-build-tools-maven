a1f8372a2344479799023691a3005723fc3e0ca6
ElinaAndreeva <lolaclouds@yandex.ru>
2023-04-24 12:08:58 +0400

    Completed the task

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,7 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (str == null || str.trim().length() == 0 || str.equals("null")) return false;
+        return Double.parseDouble(str) > 0;
     }
 }
